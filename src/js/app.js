2afafd7eea88cc21be221cdbc91c3f59f55a1f6f
Tomilov Sergey <tomexpert@yandex.ru>
2021-12-19 22:23:48 +0800

    fix

--- src/js/app.js
+++ src/js/app.js
@@ -2,7 +2,7 @@ import Chat from './chat';
 
 const domElmt = document.querySelector('.chat');
 
-const serverPath = 'ws://ahj-chat.herokuapp.com/';
+const serverPath = 'wss://ahj-chat.herokuapp.com/';
 // const serverPath = 'ws://localhost:7070';
 
 const chat = new Chat(domElmt, serverPath);
